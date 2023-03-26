867e132aa13b80f68cd4efcb9ece9490d1edf0b8
Kamil Preweda <preweda.kamil@o2.pl>
2023-03-26 15:07:40 +0200

    Implemented all CRUD operations in CreateAPIEndpoint function using axios Participant can be created now

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import { Button, Card, CardContent, TextField, Typography } from '@mui/material'
 import { Box } from '@mui/system';
 import Center from './Center';
 import useForm from './hooks/useForm';
+import { createAPIEndpoint, ENDPOINTS } from '../api';
 
 const getFreshModel = () => ({
     name: '',
@@ -21,7 +22,10 @@ export default function Login() {
     const login = e => {
         e.preventDefault()
         if (validate())
-            console.log(values);
+            createAPIEndpoint(ENDPOINTS.participant)
+                .post(values)
+                .then(res => console.log(res))
+                .catch(err => console.log(err))
     }
 
     const validate = () => {
